4b76c494d72cbea506d5b73a47732a22bd3f4c54
Rafael Vieira <rafavs09@gmail.com>
2025-01-28 11:27:15 -0300

    Save progress

--- app/src/main/java/com/example/question_game/GameActivity.java
+++ app/src/main/java/com/example/question_game/GameActivity.java
@@ -1,31 +1,54 @@
 package com.example.question_game;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class GameActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_activity);
 
-       RadioButton answer_one = findViewById(R.id.button_one_answer);
-       RadioButton answer_two = findViewById(R.id.button_two_answer);
-       RadioButton answer_three = findViewById(R.id.button_three_answer);
-       Button answer_four = findViewById(R.id.button_four_answer);
+        RadioGroup radio_group = findViewById(R.id.answers);
+        radio_group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton button = (RadioButton) radio_group.findViewById(checkedId);
+                String resposta = button.getText().toString();
+            }
+        });
+
+        RadioButton button1 = findViewById(R.id.button_one_answer);
+        button1.setText("aa");
 
 
-        answer_one.setOnClickListener(View -> Toast.makeText(this,"teste",Toast.LENGTH_SHORT).show());
+        Button next_q = findViewById(R.id.next_question);
+        next_q.setOnClickListener(View ->{
+            Intent next_question = new Intent(this, GameActivity.class);
+            startActivity(next_question, savedInstanceState);
 
+        });
+
+        TextView question_screen = findViewById(R.id.question_text);
+        question_screen.setText();
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        
-    }
+    ArrayList<String> words_array = new ArrayList<>();
+
 }
+
---
+++ app/src/main/java/com/example/question_game/question_answers.java
@@ -0,0 +1,24 @@
+package com.example.question_game;
+
+public class question_answers {
+
+   /* public static String[] questions = {
+            "Qual é o nome do ator que interpreta o pai de Sheldon, George Cooper Sr.?",
+            "Quando Penny e Leonard se casam, onde é realizada a cerimônia?",
+            "Qual é o nome da irmã de Sheldon?",
+            "O que Howard fez para ganhar seu primeiro \"bilhete\" para o espaço?",
+            "Qual é o nome do personagem que trabalha como gerente de loja de quadrinhos e tem uma paixão por Raj?",
+            "Qual era o nome do namorado da Penny, antes de Leonard?",
+            "Qual é a verdadeira profissão de Amy Farrah Fowler?"
+
+    };
+}
+    public static String[][] choices = {
+        {"Lance Barber","Jerry O'Connell","Brian Posehn","Keith Ferguson"},
+        {"Em Las Vegas","Em um hotel em Pasadena","No apartamento de Sheldon","Na casa de Amy"},
+        {"Missy Cooper","Jenny Cooper","Sarah Cooper","Lucy Cooper"},
+        {"Enviou um e-mail para a NASA","Fez um projeto de engenharia para a NASA","Fui voluntário para uma missão em Marte","Ingressou em um curso avançado de astrofísica"},
+        {"Stuart Bloom","Barry Kripke","Wil Wheaton","Howard Wolowitz"},
+        {"Zack","Kurt","Todd","Josh"},
+
+    */}
